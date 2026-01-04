624e586014601b4755e7a545f4122841bc897002
Spaxe <182298766+spaxe-dev@users.noreply.github.com>
2026-01-05 04:57:40 +0530

    feature: adds undo command

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import click
-from sorter import runSort
+from sorter import runSort, undorun
 from pathlib import Path
-@click.command()
+
+@click.group()
+
+def cli():
+    """Smart Storage CLI"""
+    pass
+
+# sort command
+@cli.command()
 @click.option('--source','-src','-s', default="input/", help='Sets source directory to be sorted.')
 @click.option('--target','-t', default="sorted/", help='Sets destination directory of sorted files.')
 @click.option('--dry', is_flag=True, help='Runs the sorter in dry mode for debugging')
-def smartsort(source, target,dry):
+def sort(source, target,dry):
     runSort(
         source = Path(source),
         target = Path(target),
         dry = dry
     )
 
-smartsort()
+
+#undo command
+@cli.command()
+def undo():
+    undorun()
+
+cli()
--- sorter.py
+++ sorter.py
@@ -1,16 +1,19 @@
 import json
-import argparse
 from pathlib import Path
 
 # configs
 
 configPath = Path("config.json")
+undolog = Path("sortlog.json")
 
 #initialization
 def loadconfig() -> dict:
     with configPath.open("r", encoding="utf-8") as f:
         config = json.load(f)
     return config
+
+def resetlog():
+    undolog.write_text("[]", encoding="utf-8")
 
 #mapping \
 def buildClassMap(config: dict) -> dict:
@@ -31,7 +34,6 @@ def extractfile(sourcedir: Path, targetdir: Path, classMap:dict, dry_run: bool):
                 
             
             
-
 def fclassify(fpath :Path, classMAP: dict) -> str:
     extType = fpath.suffix.lower().lstrip(".")
     return classMAP.get(extType, "others")
@@ -39,17 +41,47 @@ def fclassify(fpath :Path, classMAP: dict) -> str:
 
 def move(fpath: Path, targetdir: Path, classMap: dict, dry_run: bool):
     folder = fclassify(fpath, classMap)
-    destination = targetdir / folder
+    destination = targetdir / folder/ fpath.name
 
     if dry_run:
         print(f"[DRY-RUN] {fpath} --> {destination}")
         return
     
-    destination.mkdir(parents=True, exist_ok=True)
-    fpath.rename(destination/fpath.name)
+    destination.parent.mkdir(parents=True, exist_ok=True)
+    fpath.rename(destination)
+
+    move_log(fpath.resolve(), destination.resolve())
     
 def runSort(source: Path, target: Path, dry: bool = False):
     config = loadconfig()
     classMAP = buildClassMap(config)
-     
+    if not dry:
+        resetlog()
     extractfile(sourcedir=source, targetdir=target, classMap=classMAP, dry_run=dry)
+
+def move_log(src: Path, dst: Path):
+    data = json.loads(undolog.read_text(encoding="utf-8"))
+    data.append({
+        "from": str(src.resolve()),
+        "to": str(dst.resolve())
+    })
+    text = json.dumps(data)
+    undolog.write_text(text, encoding="utf-8")
+
+def undorun():
+    
+    if not undolog.exists():
+        print("Undo log doesnt exist, unable to undo.")
+
+    data = json.loads(undolog.read_text(encoding="utf-8"))
+    
+    for entry in reversed(data):
+        src = Path(entry["to"])
+        dst = Path(entry["from"])
+
+        if not src.exists():
+            print(f"[SKIPPED] Missing File: {src}")
+            continue
+        
+        dst.parent.mkdir(parents=True, exist_ok=True)
+        src.rename(dst)
